926b6b08ed24d00a6471733123a009a3d28cccaf
mhigham <mhigham@stlawu.edu>
2022-10-05 16:00:56 -0400

    class on October 5.

--- feedback_code.R
+++ feedback_code.R
@@ -6,7 +6,8 @@ heatmap_df <- tibble(challenge = c(3, 3, 1, 2, 4, 3, 2, 3, 2,
                               3, 1, 3, 1, 3, 2, 3, 2, 3,
                               1, 2, 2, 2, 1)) |>
   mutate(challenge = as.factor(challenge),
-         time = as.factor(time))
+         time = as.factor(time)) |>
+  mutate(time = fct_rev(fct_expand(time, "4")))
 
 heatmap_sum <- heatmap_df |> 
   group_by(challenge, time, .drop = FALSE) |>
@@ -17,5 +18,3 @@ ggplot(heatmap_sum, aes(x = challenge, y = time,
   geom_tile() +
   scale_fill_viridis_c(direction = 1) +
   theme_minimal()
-
-## https://r-graph-gallery.com/79-levelplot-with-ggplot2.html
